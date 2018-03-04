d5be870c04b182caac054f280c3526f0354b88c0
Edvard <edvard1807@gmail.com>
2018-03-04 21:51:14 +0200

    Add files via upload

---
+++ Generator/converter.py
@@ -0,0 +1,528 @@
+# Binary Converter
+# Converts letters to binary and vise versa
+# 1.0
+# Edvard Busck-Nielsen, hereby disclaims all copyright interest in the program “BinaryConverter” (which convert binary to letters and vise versa) written by Edvard Busck-Nielsen.
+# © Copyright 2018, Edvard Busck-Nielsen
+# signature of Edvard Busck-Nielsen 4 March 2018
+# Edvard Busck-Nielsen,
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+
+def start():
+	# Header
+	os.system('clear')
+	print "\033[4m\033[1mBinary Converter\033[0m"
+	print "\033[3m"
+	# Gets user input
+	variable = raw_input(": ")
+	# Checks what the user typed and shows corresponding output
+
+	# A
+
+	if variable == "01100001":
+		print "a"
+		raw_input()
+		start()
+	elif variable == "01000001":
+		print "A"
+		raw_input()
+		start()
+	elif variable == "a":
+		print "01100001"
+		raw_input()
+		start()
+	elif variable == "A":
+		print "01000001"
+		raw_input()
+		start()
+
+	# B
+
+	elif variable == "01100010":
+		print "b"
+		raw_input()
+		start()
+	elif variable == "01000010":
+		print "B"
+		raw_input()
+		start()
+	elif variable == "b":
+		print "01100010"
+		raw_input()
+		start()
+	elif variable == "B":
+		print "01000010"
+		raw_input()
+		start()
+
+	# C
+
+	elif variable == "01100011":
+		print "c"
+		raw_input()
+		start()
+	elif variable == "01000011":
+		print "C"
+		raw_input()
+		start()
+	elif variable == "c":
+		print "01100011"
+		raw_input()
+		start()
+	elif variable == "C":
+		print "01000011"
+		raw_input()
+		start()
+
+	# D
+
+	elif variable == "01100100":
+		print "d"
+		raw_input()
+		start()
+	elif variable == "01000100":
+		print "D"
+		raw_input()
+		start()
+	elif variable == "d":
+		print "01100100"
+		raw_input()
+		start()
+	elif variable == "D":
+		print "01000100"
+		raw_input()
+		start()
+
+	# E
+
+	elif variable == "01100101":
+		print "e"
+		raw_input()
+		start()
+	elif variable == "01000101":
+		print "E"
+		raw_input()
+		start()
+	elif variable == "e":
+		print "01100101"
+		raw_input()
+		start()
+	elif variable == "E":
+		print "01000101"
+		raw_input()
+		start()
+
+	# F
+
+	elif variable == "01100110":
+		print "f"
+		raw_input()
+		start()
+	elif variable == "01000110":
+		print "F"
+		raw_input()
+		start()
+	elif variable == "f":
+		print "01100110"
+		raw_input()
+		start()
+	elif variable == "F":
+		print "01000110"
+		raw_input()
+		start()
+
+	# G
+
+	elif variable == "01100111":
+		print "g"
+		raw_input()
+		start()
+	elif variable == "01000111":
+		print "G"
+		raw_input()
+		start()
+	elif variable == "g":
+		print "01100111"
+		raw_input()
+		start()
+	elif variable == "G":
+		print "01000111"
+		raw_input()
+		start()
+
+	# H
+
+	elif variable == "01101000":
+		print "h"
+		raw_input()
+		start()
+	elif variable == "01001000":
+		print "H"
+		raw_input()
+		start()
+	elif variable == "h":
+		print "01101000"
+		raw_input()
+		start()
+	elif variable == "H":
+		print "01001000"
+		raw_input()
+		start()
+
+	# I
+
+	elif variable == "01101001":
+		print "i"
+		raw_input()
+		start()
+	elif variable == "01001001":
+		print "I"
+		raw_input()
+		start()
+	elif variable == "i":
+		print "01101001"
+		raw_input()
+		start()
+	elif variable == "I":
+		print "01001001"
+		raw_input()
+		start()
+
+	# J
+
+	elif variable == "01101010":
+		print "j"
+		raw_input()
+		start()
+	elif variable == "01001010":
+		print "J"
+		raw_input()
+		start()
+	elif variable == "j":
+		print "01101010"
+		raw_input()
+		start()
+	elif variable == "J":
+		print "01001010"
+		raw_input()
+		start()
+
+	# K
+
+	elif variable == "01101011":
+		print "k"
+		raw_input()
+		start()
+	elif variable == "01001011":
+		print "K"
+		raw_input()
+		start()
+	elif variable == "k":
+		print "01101011"
+		raw_input()
+		start()
+	elif variable == "K":
+		print "01001011"
+		raw_input()
+		start()
+
+	# L
+
+	elif variable == "01101100":
+		print "l"
+		raw_input()
+		start()
+	elif variable == "01001100":
+		print "L"
+		raw_input()
+		start()
+	elif variable == "l":
+		print "01101100"
+		raw_input()
+		start()
+	elif variable == "L":
+		print "01001100"
+		raw_input()
+		start()
+
+	# M
+
+	elif variable == "01101101":
+		print "m"
+		raw_input()
+		start()
+	elif variable == "01001101":
+		print "M"
+		raw_input()
+		start()
+	elif variable == "m":
+		print "01101101"
+		raw_input()
+		start()
+	elif variable == "M":
+		print "01001101"
+		raw_input()
+		start()
+
+	# N
+
+	elif variable == "01101110":
+		print "n"
+		raw_input()
+		start()
+	elif variable == "01001110":
+		print "N"
+		raw_input()
+		start()
+	elif variable == "n":
+		print "01101110"
+		raw_input()
+		start()
+	elif variable == "N":
+		print "01001110"
+		raw_input()
+		start()
+
+	# O
+
+	elif variable == "01101111":
+		print "o"
+		raw_input()
+		start()
+	elif variable == "01001111":
+		print "O"
+		raw_input()
+		start()
+	elif variable == "o":
+		print "01101111"
+		raw_input()
+		start()
+	elif variable == "O":
+		print "01001111"
+		raw_input()
+		start()
+
+	# P
+
+	elif variable == "01110000":
+		print "p"
+		raw_input()
+		start()
+	elif variable == "01010000":
+		print "P"
+		raw_input()
+		start()
+	elif variable == "p":
+		print "01110000"
+		raw_input()
+		start()
+	elif variable == "P":
+		print "01010000"
+		raw_input()
+		start()
+
+	# Q
+
+	elif variable == "01110001":
+		print "q"
+		raw_input()
+		start()
+	elif variable == "01010001":
+		print "Q"
+		raw_input()
+		start()
+	elif variable == "q":
+		print "01110001"
+		raw_input()
+		start()
+	elif variable == "Q":
+		print "01010001"
+		raw_input()
+		start()
+
+	# R
+
+	elif variable == "01110010":
+		print "r"
+		raw_input()
+		start()
+	elif variable == "01010010":
+		print "R"
+		raw_input()
+		start()
+	elif variable == "r":
+		print "01110010"
+		raw_input()
+		start()
+	elif variable == "R":
+		print "01010010"
+		raw_input()
+		start()
+
+	# S
+
+	elif variable == "01110011":
+		print "s"
+		raw_input()
+		start()
+	elif variable == "01010011":
+		print "S"
+		raw_input()
+		start()
+	elif variable == "s":
+		print "01110011"
+		raw_input()
+		start()
+	elif variable == "S":
+		print "01010011"
+		raw_input()
+		start()
+
+	# T
+
+	elif variable == "01110100":
+		print "t"
+		raw_input()
+		start()
+	elif variable == "01010100":
+		print "T"
+		raw_input()
+		start()
+	elif variable == "t":
+		print "01110100"
+		raw_input()
+		start()
+	elif variable == "T":
+		print "01010100"
+		raw_input()
+		start()
+
+	# U
+
+	elif variable == "01110101":
+		print "u"
+		raw_input()
+		start()
+	elif variable == "01010101":
+		print "U"
+		raw_input()
+		start()
+	elif variable == "u":
+		print "01110101"
+		raw_input()
+		start()
+	elif variable == "U":
+		print "01010101"
+		raw_input()
+		start()
+
+	# V
+
+	elif variable == "01110110":
+		print "v"
+		raw_input()
+		start()
+	elif variable == "01010110":
+		print "V"
+		raw_input()
+		start()
+	elif variable == "v":
+		print "01110110"
+		raw_input()
+		start()
+	elif variable == "V":
+		print "01010110"
+		raw_input()
+		start()
+
+	# W
+
+	elif variable == "01110111":
+		print "w"
+		raw_input()
+		start()
+	elif variable == "01010111":
+		print "W"
+		raw_input()
+		start()
+	elif variable == "w":
+		print "01110111"
+		raw_input()
+		start()
+	elif variable == "W":
+		print "01010111"
+		raw_input()
+		start()
+
+	# X
+
+	elif variable == "01111000":
+		print "x"
+		raw_input()
+		start()
+	elif variable == "01011000":
+		print "X"
+		raw_input()
+		start()
+	elif variable == "x":
+		print "01111000"
+		raw_input()
+		start()
+	elif variable == "X":
+		print "01011000"
+		raw_input()
+		start()
+
+	# Y
+
+	elif variable == "01111001":
+		print "y"
+		raw_input()
+		start()
+	elif variable == "01011001":
+		print "Y"
+		raw_input()
+		start()
+	elif variable == "y":
+		print "01111001"
+		raw_input()
+		start()
+	elif variable == "Y":
+		print "01011001"
+		raw_input()
+		start()
+
+	# Z
+
+	elif variable == "01111010":
+		print "z"
+		raw_input()
+		start()
+	elif variable == "01011010":
+		print "Z"
+		raw_input()
+		start()
+	elif variable == "z":
+		print "01111010"
+		raw_input()
+		start()
+	elif variable == "Z":
+		print "01011010"
+		raw_input()
+		start()
+
+start()
